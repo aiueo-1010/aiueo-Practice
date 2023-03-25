6e659dfd10e1cf18d0667f4f375456747a9a8656
aiueo-1010 <fal.aiueo.fripside.1010@gmail.com>
2023-03-26 04:50:37 +0900

    a

--- AITSHP/index.php
+++ AITSHP/index.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="ja">
     <head>
-        <?php include('./head.html') ?>
+        <?php include('head.html') ?>
 
         <title>ホーム - 株式会社AITS</title>
     </head>
     <body>
-        <div class="wrapper">
+
             <!-- header include -->
-            <?php include('./header.html') ?>
+            <?php include('header.html') ?>
             <!-- ↑この１行はphp、読み込んでるのはhtml -->
 
-            <!-- ページごとに変わるコンテンツ部分を記述 -->
-            <div class="content">
+            <!-- ナビゲーションバー -->
+            <div class="">
 
             </div>
         
             <!-- footer include -->
-            <?php include('./footer.html') ?>
+            <?php include('footer.html') ?>
             <!-- ↑この１行はphp、読み込んでるのはhtml -->
-        </div>
+
     </body>
 </html>
