974235008ba6f096a692dcdf13de3370759bd0a5
aiueo-1010 <fal.aiueo.fripside.1010@gmail.com>
2023-04-06 01:34:46 +0900

    aasas

--- AITSHP/index.php
+++ AITSHP/index.php
@@ -7,13 +7,48 @@
     </head>
     <body>
 
-            <!-- header include -->
-            <?php include('header.html') ?>
-            <!-- ↑この１行はphp、読み込んでるのはhtml -->
+        <!-- header include -->
+        <?php include('header.html') ?>
+        <!-- ↑この１行はphp、読み込んでるのはhtml -->
 
-            <!-- footer include -->
-            <?php include('footer.html') ?>
-            <!-- ↑この１行はphp、読み込んでるのはhtml -->
+        <div class="tabs">
+            <ul >
+                <li>
+                    <a href="">
+                        <div class="tab_item background-color">ホーム</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="Solution/Solution_index.html">
+                        <span class="tab_item">事業内容</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="Guide/Guide_index.html">
+                        <div class="tab_item">会社案内</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="aisatu/aisatu_index.html">
+                        <div class="tab_item">ご挨拶</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="Contact/Contact_index.html">
+                        <div class="tab_item">お問い合わせ</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="Careers/Careers_index.html">
+                        <div class="tab_item">採用情報</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- footer include -->
+        <?php include('footer.html') ?>
+        <!-- ↑この１行はphp、読み込んでるのはhtml -->
 
     </body>
 </html>
