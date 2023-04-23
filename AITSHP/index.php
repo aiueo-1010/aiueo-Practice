7505d65614e34dff393bb8b4d12f08728938fe66
aiueo-1010 <fal.aiueo.fripside.1010@gmail.com>
2023-04-24 05:24:40 +0900

    11

--- AITSHP/index.php
+++ AITSHP/index.php
@@ -12,10 +12,10 @@
         <!-- ↑この１行はphp、読み込んでるのはhtml -->
 
         <div class="tabs">
-            <ul >
+            <ul>
                 <li>
                     <a href="">
-                        <div class="tab_item background-color">ホーム</div>
+                        <span class="Selected_item">ホーム</span>
                     </a>
                 </li>
                 <li>
@@ -25,30 +25,73 @@
                 </li>
                 <li>
                     <a href="Guide/Guide_index.html">
-                        <div class="tab_item">会社案内</div>
+                        <span class="tab_item">会社案内</span>
                     </a>
                 </li>
                 <li>
                     <a href="aisatu/aisatu_index.html">
-                        <div class="tab_item">ご挨拶</div>
+                        <span class="tab_item">ご挨拶</span>
                     </a>
                 </li>
                 <li>
                     <a href="Contact/Contact_index.html">
-                        <div class="tab_item">お問い合わせ</div>
+                        <span class="tab_item">お問い合わせ</span>
                     </a>
                 </li>
                 <li>
                     <a href="Careers/Careers_index.html">
-                        <div class="tab_item">採用情報</div>
+                        <span class="tab_item">採用情報</span>
                     </a>
                 </li>
             </ul>
         </div>
 
+        <div>
+            <p>
+                &nbsp;
+            </p>
+            <p>
+                &nbsp;
+            </p>
+            <p style="text-align: center; margin: 7px 0;">
+                <span style="font-size: 16px; font-family: Arial;">
+                    弊社では お客様のニーズに合わせて様々な
+                </span>
+            </p>
+            <p>
+                &nbsp;
+            </p>
+            <p style="text-align: center; margin: 7px 0;">
+                <span style="font-size: 16px; font-family: Arial;">
+                    技術&nbsp; 　 知識&nbsp; &nbsp;　経験
+                </span>
+            </p>
+            <p>
+                &nbsp;
+            </p>
+            <p style="text-align: center; margin: 7px 0;">
+                <span style="font-size: 16px; font-family: Arial;">
+                をご提供致します
+                </span>
+            </p>
+            <p>
+                &nbsp;
+            </p>
+            <p style="text-align: center; margin: 7px 0;">
+                <span style="font-size: 16px; font-family: Arial;">
+                お気軽にお問い合わせください
+                </span>
+            <p>
+                &nbsp;
+            </p>
+            <p>
+                &nbsp;
+            </p>
+        </div>
+
         <!-- footer include -->
         <?php include('footer.html') ?>
         <!-- ↑この１行はphp、読み込んでるのはhtml -->
-
+        
     </body>
 </html>
